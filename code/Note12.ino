bdaa5201419c2c1b0aba4a8a67b934387d1466a3
pandey-Rajroshan <rajroshanp05@gmail.com>
2025-04-01 20:31:40 +0530

    Add files via upload

---
+++ code/Note12.ino
@@ -0,0 +1,33 @@
+#define BLYNK_TEMPLATE_ID "TMPL3f_WaWj8w"
+#define BLYNK_TEMPLATE_NAME "Home Automation"
+#define BLYNK_AUTH_TOKEN "jtDIi_o-LTY5z5rfQZxF6cn4xRNzSYVn"
+
+
+  #include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+char ssid[] = "NOTE12";
+char pass[] = "wohhihai";
+
+
+#define RELAY_PIN D4  // Change to the GPIO pin connected to your relay/LED
+
+BLYNK_WRITE(V1) { // Virtual pin V1
+    int switchState = param.asInt(); // Read button state
+    digitalWrite(RELAY_PIN, switchState); // Control relay
+    
+}
+
+void setup() {
+    pinMode(RELAY_PIN, OUTPUT);
+    digitalWrite(RELAY_PIN, LOW); // Start with relay off
+    
+    Serial.begin(9600);
+    Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
+}
+
+void loop() {
+    Blynk.run();
+    
+    
+}
